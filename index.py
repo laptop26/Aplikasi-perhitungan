642f94758d828369dc5ea12142f6ad32afad9381
laptop26 <85729402+laptop26@users.noreply.github.com>
2022-11-10 21:16:40 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,36 +1,13 @@
-def add(x, y):
-   return x + y
-
-def subtract(x, y):
-   return x - y
-
-def multiply(x, y):
-   return x * y
-
-def divide(x, y):
-   return x / y
-
-print("Pilih Operasi.")
-print("1.Jumlah")
-print("2.Kurang")
-print("3.Kali")
-print("4.Bagi")
-
-choice = input("Masukkan pilihan(1/2/3/4): ")
-
-num1 = int(input("Masukkan bilangan pertama: "))
-num2 = int(input("Masukkan bilangan kedua: "))
-
-if choice == '1':
-   print(num1,"+",num2,"=", add(num1,num2))
-
-elif choice == '2':
-   print(num1,"-",num2,"=", subtract(num1,num2))
-
-elif choice == '3':
-   print(num1,"*",num2,"=", multiply(num1,num2))
-
-elif choice == '4':
-   print(num1,"/",num2,"=", divide(num1,num2))
-else:
-   print("Input salah")
+from itertools import combinations
+ 
+data_list = [1, 2, 3, 4, 5]
+ 
+kombinasi = combinations(data_list, r=2)
+ 
+# cetak objects
+print('object kombinasi', kombinasi)
+ 
+ 
+# Cetak Hasil
+for index, data in enumerate(list(kombinasi), 1):
+    print(f'kombinasi ke: {index}. {data}')
